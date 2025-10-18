9f475aa6acce73d2fbea5b3899a8de87dcb25677
Avishek Dutta <avishekdutta531@gmail.com>
2025-10-18 18:53:18 +0600

    update link

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nsu-sobers/',
   plugins: [tailwindcss()],
 })
